788238a5bc5b93430e0a58a14ecb52fcd4040379
Seth <sethtan845@gmail.com>
2022-12-03 15:28:09 +0800

    adjustments

---
+++ reviews.js
@@ -0,0 +1,21 @@
+let logo = document.querySelector(".nav-logo");
+logo.addEventListener("mouseover", mouseOver);
+
+function mouseOver() {
+  menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+
+  promotions = document.querySelector(".promotions");
+  promotions.classList.toggle("active");
+
+  reviews = document.querySelector(".reviews");
+  reviews.classList.toggle("active");
+
+  contacts = document.querySelector(".contacts");
+  contacts.classList.toggle("active");
+
+  membership = document.querySelector(".membership");
+  membership.classList.toggle("active");
+}
+
+AOS.init();
